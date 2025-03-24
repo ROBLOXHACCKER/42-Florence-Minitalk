99ad0b1906994f253141a0c4704dc375e522ee8b
Paolo Brogi <hurasmi@Paolos-MacBook-Air.local>
2025-03-24 12:39:42 +0100

    finito server

--- server.c
+++ server.c
@@ -6,26 +6,23 @@
 /*   By: hurasmi <hurasmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 11:10:22 by pbrogi            #+#    #+#             */
-/*   Updated: 2025/03/19 17:13:23 by hurasmi          ###   ########.fr       */
+/*   Updated: 2025/03/24 12:39:27 by hurasmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/utils_server.h"
 
-int	main()
+int	main(void)
 {
 	int		server_pid;
-	
+
 	server_pid = getpid();
 	ft_printf("Server PID: %d\n\n", server_pid);
-	
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
-	
-	while(1)
+	while (1)
 	{
 		pause();
 	}
-    
-	return 0;
+	return (0);
 }
