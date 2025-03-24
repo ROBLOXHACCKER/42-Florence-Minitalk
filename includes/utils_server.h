1d0306011615b11a76baccaf9d7a7fb1024dc693
Paolo Brogi <hurasmi@Paolos-MacBook-Air.local>
2025-03-24 13:42:38 +0100

    utils_server.c norminette finito

--- includes/utils_server.h
+++ includes/utils_server.h
@@ -6,57 +6,57 @@
 /*   By: hurasmi <hurasmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 17:00:32 by pbrogi            #+#    #+#             */
-/*   Updated: 2025/03/19 17:22:59 by hurasmi          ###   ########.fr       */
+/*   Updated: 2025/03/24 13:42:13 by hurasmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef UTILS_SERVER_H
-#define UTILS_SERVER_H
+# define UTILS_SERVER_H
 
-#include "../includes/ft_printf/ft_printf.h"
-#include "../includes/libft/libft.h"
-#include <signal.h>
+# include "../includes/ft_printf/ft_printf.h"
+# include "../includes/libft/libft.h"
+# include <signal.h>
 
-int pow_two(int exponent)
+int	pow_two(int exponent)
 {
-    int base = 1;
-    
-    while (exponent > 0) {
-        base *= 2;
-        exponent--;
-    }
+	int		base;
 
-    return base;
+	base = 1;
+	while (exponent > 0)
+	{
+		base *= 2;
+		exponent--;
+	}
+	return (base);
 }
 
-void signal_handler(int sig)
+void	signal_handler(int sig)
 {
-    static int i = 7;
-    static int ascii = 0;
-    static char message[1024];
-    static int index = 0;
+	static int	i;
+	static int	ascii;
+	static char	message[1024];
+	static int	index;
 
-    if (sig == SIGUSR2)
-        ascii += pow_two(i);
-
-    if (i == 0)
-    {
-        message[index] = ascii;
-        index++;
-
-        if (ascii == 0)
-        {
-            message[index] = '\0';
-            ft_printf("%s\n", message);
-            index = 0;
-        }
-
-        i = 7;
-        ascii = 0;
-    }
-    else
-        i--;
+	i = 7;
+	index = 0;
+	ascii = 0;
+	if (sig == SIGUSR2)
+		ascii += pow_two(i);
+	if (i == 0)
+	{
+		message[index] = ascii;
+		index++;
+		if (ascii == 0)
+		{
+			message[index] = '\0';
+			ft_printf("%s\n", message);
+			index = 0;
+		}
+		i = 7;
+		ascii = 0;
+	}
+	else
+		i--;
 }
 
 #endif
